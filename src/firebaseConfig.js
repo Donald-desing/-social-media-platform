d90842c215e2890737e9fa8cf09f81d87c1d71ca
Donald-Design <79831132+Donald-desing@users.noreply.github.com>
2025-02-21 08:59:32 +0300

    Create firebaseConfig.js

---
+++ src/firebaseConfig.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyABLIr75CKN1qw0_wEcWj4xWVEMev33mMo",
+  authDomain: "social-media-platform-3555d.firebaseapp.com",
+  projectId: "social-media-platform-3555d",
+  storageBucket: "social-media-platform-3555d.firebasestorage.app",
+  messagingSenderId: "58212375662",
+  appId: "1:58212375662:web:ae8e9cbbb1549f019eda7f"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
