cd3492a2e4cf8935e4a66b20833778729f7affe6
Donald-Design <79831132+Donald-desing@users.noreply.github.com>
2025-02-21 09:07:24 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,18 +1,38 @@
 // src/App.js
 
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { onAuthStateChanged } from 'firebase/auth';
+import { auth } from './firebaseConfig';
 import Register from './Register';
 import Login from './Login';
 import HomePage from './HomePage';
+import Dashboard from './Dashboard';
 
 function App() {
+  const [user, setUser] = useState(null);
+
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
+      setUser(currentUser);
+    });
+    return () => unsubscribe();
+  }, []);
+
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/register" element={<Register />} />
-        <Route path="/login" element={<Login />} />
+        {user ? (
+          <>
+            <Route path="/" element={<Dashboard />} />
+          </>
+        ) : (
+          <>
+            <Route path="/" element={<HomePage />} />
+            <Route path="/register" element={<Register />} />
+            <Route path="/login" element={<Login />} />
+          </>
+        )}
       </Routes>
     </Router>
   );
