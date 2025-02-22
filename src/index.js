c33c6beaed589c1d35d79f79aa0f56fb3b79d879
Donald-Design <79831132+Donald-desing@users.noreply.github.com>
2025-02-22 14:17:18 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -3,13 +3,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import { BrowserRouter } from 'react-router-dom';
+import ErrorBoundary from './ErrorBoundary';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <ErrorBoundary>
       <App />
-    </BrowserRouter>
+    </ErrorBoundary>
   </React.StrictMode>
 );
