f0fc85ca7bd3f86fc2dd4e8bec989ef44017e733
Donald-Design <79831132+Donald-desing@users.noreply.github.com>
2025-02-24 17:02:08 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 // src/index.js
-
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import ErrorBoundary from './ErrorBoundary';
+import './ErrorBoundary.css'; // Import CSS
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
