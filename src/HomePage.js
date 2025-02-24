857312f291f91f368800a4d67b14da6496674fcd
Donald-Design <79831132+Donald-desing@users.noreply.github.com>
2025-02-24 16:48:34 +0300

    Update HomePage.js

--- src/HomePage.js
+++ src/HomePage.js
@@ -1,11 +1,14 @@
 // src/HomePage.js
 import React from 'react';
 import { NavLink } from 'react-router-dom';
+import './HomePage.css';
 
 function HomePage() {
   return (
-    <div>
+    <div className="home-page-container">
       <h1>Welcome to the Social Media Platform</h1>
+      <p>Connect with friends, share your thoughts, and discover new communities.</p> {/* Added description */}
+      {/* <img src="/path/to/your/image.jpg" alt="Social Media Platform" />  Example image */}
       <NavLink to="/register">Register</NavLink>
       <br />
       <NavLink to="/login">Login</NavLink>
